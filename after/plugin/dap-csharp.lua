35891e69c6f6c45360908ed20f19999f2f60e206
RAZ COHEN <razcohen@cellcom.co.il>
2024-09-26 16:08:10 +0300

    feat(dap-csharp): add preLaunchTask and attach config

--- after/plugin/dap-csharp.lua
+++ after/plugin/dap-csharp.lua
@@ -2,11 +2,13 @@ local dap = require('dap')
 
 -- Set noshellslash for Windows
 vim.cmd('set noshellslash')
+
 dap.adapters.coreclr = {
     type = 'executable',
     command = os.getenv('USERPROFILE') .. '/scoop/apps/netcoredbg/current/netcoredbg.exe',
     args = { '--interpreter=vscode' }
 }
+
 dap.configurations.cs = {
     {
         type = "coreclr",
@@ -15,5 +17,21 @@ dap.configurations.cs = {
         program = function()
             return vim.fn.input('Path to dll: ', vim.fn.getcwd() .. '/bin/Debug/', 'file')
         end,
+        preLaunchTask = function()
+            local solution_path = vim.fn.input('Path to solution: ', vim.fn.getcwd() .. '/', 'file')
+            vim.cmd('!dotnet build ' .. solution_path)
+        end,
+    },
+    {
+        type = "coreclr",
+        name = "attach - netcoredbg",
+        request = "attach",
+        processId = require('dap.utils').pick_process,
+        sourceFileMap = {
+            ['/path/to/your/source'] = '${workspaceFolder}/path/to/your/source'
+        },
     },
 }
+
+-- Enable logging for debugging
+dap.set_log_level('DEBUG')
