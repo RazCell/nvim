adbbc7f598151a92c90286bc2cf6dde0233c0e03
RAZ COHEN <razcohen@cellcom.co.il>
2024-11-12 14:53:33 +0200

    feat: update copilot-chat and null-ls configurations
    - Changed `auto_follow_cursor` to `false` in copilot-chat.lua
    - Set timeout to 5000ms for formatting in null-ls.lua
    - Removed redundant keymap setting for formatting in null-ls.lua

--- after/plugin/copilot-chat.lua
+++ after/plugin/copilot-chat.lua
@@ -21,7 +21,7 @@ require("CopilotChat").setup({
 
     show_folds = true, -- Shows folds for sections in chat
     show_help = true, -- Shows help message as virtual lines when waiting for user input
-    auto_follow_cursor = true, -- Auto-follow cursor in chat
+    auto_follow_cursor = false, -- Auto-follow cursor in chat
     auto_insert_mode = false, -- Automatically enter insert mode when opening window and on new prompt
     insert_at_end = false, -- Move cursor to end of buffer when inserting text
     clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
